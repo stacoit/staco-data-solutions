1ee92ea5225170056682ceb21ab2a0d7c81a1fef
Dinesh R <rdineshrdp@gmail.com>
2024-11-23 12:32:09 +0530

    Add GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/staco-data-solutions',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
