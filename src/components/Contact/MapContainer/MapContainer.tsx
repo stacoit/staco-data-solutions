2ba3ddc7af52981e4717983e974bbb9fddc85682
Dinesh R <rdineshrdp@gmail.com>
2025-03-06 23:46:38 +0530

    Frontend is done

--- src/components/Contact/MapContainer/MapContainer.tsx
+++ src/components/Contact/MapContainer/MapContainer.tsx
@@ -16,10 +16,10 @@ export default function MapContainer() {
                     <h1 className={styles.mainHeading}>Visit us at our office or connect with us online.</h1>
                     <div className={styles.contactDetails}>
                         <p className={styles.addressLine}>
-                            <span className={styles.contactLabel}>Address:</span> 2712 County Line Road, St. Petersburg, FL 33701, USA
+                            <span className={styles.contactLabel}>Address:</span> 350 Holger Way, San Jose, CA 95134, USA
                         </p>
                         <p className={styles.contactLine}>
-                            <span className={styles.contactLabel}>Email:</span> contact@stacodata.com
+                            <span className={styles.contactLabel}>Email:</span> Info@stacodatasolutions.com
                         </p>
                         <p className={styles.contactLine}>
                             <span className={styles.contactLabel}>Phone:</span> +1 (555) 123-4567
@@ -28,7 +28,7 @@ export default function MapContainer() {
                 </div>
                 <div className={styles.mapContainer}>
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d112910.59274003663!2d-82.77985863716421!3d27.826510407383992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c2e19ba9c0be39%3A0x8ae47e3adb68d30a!2sSt.%20Petersburg%2C%20FL%2033701%2C%20USA!5e0!3m2!1sen!2sin!4v1741203424765!5m2!1sen!2sin"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.789603293235!2d-121.94495800000001!3d37.4184476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fc9002ee66775%3A0xbed47a3a3dd4693!2s350%20Holger%20Way%2C%20San%20Jose%2C%20CA%2095134%2C%20USA!5e0!3m2!1sen!2sin!4v1741284165421!5m2!1sen!2sin"
                         className={styles.mapFrame}
                         title="Current Place on a Map"
                         allowFullScreen
